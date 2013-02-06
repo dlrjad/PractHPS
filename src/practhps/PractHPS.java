1aeb43eb8386f5b6f1c1fbee942e65c6457d0099
jorge <jorge@jorge-K52JT>
2013-02-06 10:34:12 +0000

    SecondState

--- src/practhps/PractHPS.java
+++ src/practhps/PractHPS.java
@@ -9,12 +9,55 @@ package practhps;
  * @author jorge
  */
 public class PractHPS {
+    
+    class Nodo{
+        String info;
+        Nodo izq, der;
+    }
+    Nodo raiz;
+    
+    public PractHPS(){
+        raiz = null;
+    }
+    
+    public void insertar (String info)
+      {
+          Nodo nuevo;
+          nuevo = new Nodo ();
+          nuevo.info = info;
+          nuevo.izq = null;
+          nuevo.der = null;
+          if (raiz == null) {
+              raiz = nuevo;
+          }
+          else
+          {
+              Nodo anterior = null;
+              Nodo reco = raiz;
+              /*while (reco != null)
+              {
+                  anterior = reco;
+                  if (info < reco.info) {
+                      reco = reco.izq;
+                  }
+                  else {
+                      reco = reco.der;
+                  }
+              }
+              if (info < anterior.info) {
+                  anterior.izq = nuevo;
+              }
+              else {
+                  anterior.der = nuevo;
+              }*/
+          }
+      }
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //hola
         // TODO code application logic here
     }
 }
